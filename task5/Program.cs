9b25d960c554b21bae4971c10dac572c12f42ed3
Denis <inquis87@gmail.com>
2022-11-29 22:52:16 +0300

    деление с остатком

---
+++ task5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine());
+
+int a = 0;
+double b = 0;
+a = N%10;
+b = N/10;
+
+Console.WriteLine("деление с остатком" + a + " ");
+Console.Write("деление целое" + b + " ");
+
